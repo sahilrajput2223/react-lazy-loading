7754f2724be9920021ec6c51098a7b59054a1e84
Sahil Rajput <rajputsahil.2204@gmail.com>
2022-10-26 21:04:16 +0530

    Heavy Comp Imported as Lazy Comp

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+const HeavyComponent = React.lazy(() => import('../OtherHeavyComp/HeavyComponent'))
+
 const App = () => {
     return (
         <React.Fragment>
@@ -7,6 +9,9 @@ const App = () => {
                 <h1>
                     Hello World !!
                 </h1>
+                <React.Suspense fallback={<h2>Loding...</h2>}>
+                    <HeavyComponent />
+                </React.Suspense>
             </center>
         </React.Fragment>
     );
