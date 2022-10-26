b11deb148d33fa0107d48666d6c89d33e1f4ac2d
Sahil Rajput <rajputsahil.2204@gmail.com>
2022-10-26 19:47:13 +0530

    Hello World - Basic App Component Added

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <React.Fragment>
+            <center>
+                <h1>
+                    Hello World !!
+                </h1>
+            </center>
+        </React.Fragment>
+    );
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+import App from "./App/App"
 
-ReactDOM.render("", document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
